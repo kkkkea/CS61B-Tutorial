c7ddc70261d5212c9136f08fbbfc64f232a7470f
kkkkea <447872721@qq.com>
2023-07-20 20:22:52 +0800

    add test case for OffByone

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -12,8 +12,11 @@ public class TestOffByOne {
         assertTrue(offByOne.equalChars('a', 'b'));
         assertTrue(offByOne.equalChars('c', 'd'));
         assertTrue(offByOne.equalChars('f', 'e'));
+        assertTrue(offByOne.equalChars('&', '%'));
 
         assertFalse(offByOne.equalChars('a', 'a'));
         assertFalse(offByOne.equalChars('b', 'b'));
+        assertFalse(offByOne.equalChars('b', 'B'));
+        assertFalse(offByOne.equalChars('2', 'b'));
     }
 }
